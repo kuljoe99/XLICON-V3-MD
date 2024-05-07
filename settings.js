e1d2f499beabfd05f7b88a947238b7707d421393
kuljoe99 <johnniejohntez@gmail.com>
2024-05-08 00:14:58 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,8 +26,8 @@
 //re-upload? recode? copy code? give credit ya :)
 //Instagram: unicorn_xeon13
 //Telegram: t.me/ahmmitech
-//GitHub: @salmanytofficial
-//WhatsApp: +923184070915
+//GitHub: @kuljoe99
+//WhatsApp: +254796839274
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
 //   * Created By Github: DGXeon.
 //   * Credit To Xeon
